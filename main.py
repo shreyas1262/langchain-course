7005b6fbae734a7a84cd2683f4fe5bd4a85e10aa
Shreyas Telkar <shreyas1262@gmail.com>
2025-10-17 17:29:47 +0530

    Used structured_llm instead of pydantic output parser

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
+
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
@@ -15,8 +15,9 @@ load_dotenv()
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4")
+# This will enable the llm to output structured data as per the AgentResponse schema
+structured_llm = llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=[
@@ -25,7 +26,7 @@ react_prompt_with_format_instructions = PromptTemplate(
         "tools",
         "tool_names",
     ],
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 # creates a chain that provides the prompt and tools to the llm
 agent = create_react_agent(
@@ -35,10 +36,8 @@ agent = create_react_agent(
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 # Creates a runnable that extracts the "output" key from a dictionary
 extract_output = RunnableLambda(lambda x: x["output"])
-# Creates a runnable that parses the output using the Pydantic output parser
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
 
-chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 
 def main():
